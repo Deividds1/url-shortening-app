39241b39673f92f950c4be59adb7e912b34dac4a
GuillermoGrisi <gfg1977@gmail.com>
2023-11-15 17:10:29 -0300

    lista de url, boton copy y shortenedUrl

--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,51 +1,4 @@
-/* import React from 'react';
-import { useState } from 'react';
-import "./input.css";
-
-function Input() {
-  const [inputUrl, setinputUrl] = useState("");
-  const [newUrl, setNewUrl] = useState("");
-
-  const getUrl = (event) => {
-    setinputUrl(event.target.value)
-  }
-
-  const enviarUrl = async () => {
-    const apiUrl = `http://localhost:5000/api`;
-
-      try {
-        const response = await fetch(apiUrl, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ url: inputUrl }),
-        });
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const data = await response.json();
-        setNewUrl(data);
-        console.log(data);
-      } catch (error) {
-        console.error('Error al cargar datos:', error);
-      }
-    };
-
-  return (
-    <div className='backgroundContainer'>
-      <div className='inputContainer'>
-        <input onChange={getUrl} className='shortenInput' type="text" placeholder="Shorten a link here..." /><br />
-        <button onClick={enviarUrl} className='shortenItButton'>Shorten It!</button><br />
-      </div>
-    </div>
-  )
-}
-
-
-export default Input */
-
-import React, { useState } from 'react';
+/* import React, { useState } from 'react';
 import './input.css';
 
 function Input() {
@@ -98,6 +51,72 @@ function Input() {
         </div>
       )}
       
+    </div>
+  );
+}
+
+export default Input; */
+import React, { useState } from 'react';
+import './input.css';
+
+function Input() {
+  const [inputUrl, setInputUrl] = useState('');
+  const [shortenedUrls, setShortenedUrls] = useState([]);
+  const [showShortened, setShowShortened] = useState(false);
+
+  const getUrl = (event) => {
+    setInputUrl(event.target.value);
+  };
+
+  const enviarUrl = async () => {
+    const apiUrl = `http://localhost:5000/api`;
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ url: inputUrl }),
+      });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      const newShortenedUrl = data.result_url;
+      setShortenedUrls((prevUrls) => [
+        { originalUrl: inputUrl, shortenedUrl: newShortenedUrl },
+        ...prevUrls.slice(0, 2),
+      ]);
+      setShowShortened(true);
+    } catch (error) {
+      console.error('Error al cargar datos:', error);
+    }
+  };
+
+  const copyToClipboard = (url) => {
+    navigator.clipboard.writeText(url);
+    alert('URL copiada al portapapeles');
+  };
+
+  return (
+    <div className="backgroundContainer">
+      <div className="inputContainer">
+        <input onChange={getUrl} className="shortenInput" type="text" placeholder="Shorten a link here..." /><br />
+        <button onClick={enviarUrl} className="shortenItButton">Shorten It!</button><br />
+      </div>
+
+      {showShortened && (
+        <div className="shortenedUrlContainer">
+          {shortenedUrls.map((item, index) => (
+            <div className='originalAndShortenedContainer' key={index}>
+              <p className="originalUrl">{item.originalUrl}</p><hr></hr>
+              <p className="shortenedUrl">{item.shortenedUrl}</p>
+              <button onClick={() => copyToClipboard(item.shortenedUrl)} className="copyButton">Copy</button>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
